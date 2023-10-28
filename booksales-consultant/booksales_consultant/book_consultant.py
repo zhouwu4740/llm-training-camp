2a6ae68d1711dae4067f7bbfb2ecbdcc1eaafc3c
denniszhou <21563099@qq.com>
2023-10-28 21:54:07 +0800

    添加购物车功能

--- booksales-consultant/booksales_consultant/book_consultant.py
+++ booksales-consultant/booksales_consultant/book_consultant.py
@@ -1,19 +1,80 @@
+from operator import itemgetter
 from typing import Literal
 
-from utils import logger
-from prompt import prompt
-from pydantic.v1 import BaseModel
 from langchain.chains import RetrievalQA
-from langchain.vectorstores.faiss import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.schema.language_model import BaseLanguageModel
-from langchain.schema.embeddings import Embeddings
-from langchain.schema.runnable import Runnable, RunnablePassthrough
 from langchain.output_parsers.openai_functions import PydanticAttrOutputFunctionsParser
-from langchain.utils.openai_functions import convert_pydantic_to_openai_function
+from langchain.schema.runnable import Runnable
 from langchain.schema.runnable import RunnableBranch, RunnableLambda, RunnablePassthrough
-from operator import itemgetter
+from langchain.utils.openai_functions import convert_pydantic_to_openai_function
+from langchain.vectorstores.faiss import FAISS
+from pydantic.v1 import BaseModel
+from langchain.schema.messages import HumanMessage, AIMessage
+from langchain.prompts import ChatPromptTemplate
+
+from prompt import prompt
+from utils import logger
+from shopping_cart import finish_request
+
+
+def determine_topic(question: str) -> Literal["qa", "shopping"]:
+    """Determine the topic of the question
+    qa: Reply to user inquiries for books and provide some purchasing suggestions
+    shopping: Assist customers in managing their shopping cart, including services such as adding products,
+        deleting products, viewing inventory, and settling accounts
+    """
+    classifier_function = convert_pydantic_to_openai_function(TopicClassifier)
+    llm = ChatOpenAI(temperature=0).bind(functions=[classifier_function], function_call={"name": "TopicClassifier"})
+    parser = PydanticAttrOutputFunctionsParser(pydantic_schema=TopicClassifier, attr_name="topic")
+    classifier_chain = llm | parser
+
+    return classifier_chain.invoke(question)
+
+
+def question_answer() -> Runnable:
+    embedding = OpenAIEmbeddings()
+
+    db = FAISS.load_local('../jupyter/books', embedding)
+    retriever = db.as_retriever(
+        search_type="similarity_score_threshold",
+        search_kwargs={"score_threshold": 0.7, "k": 3})
+    llm = ChatOpenAI(temperature=0.7)
+    qa = RetrievalQA.from_chain_type(llm=llm, retriever=retriever, verbose=True)
+    qa.combine_documents_chain.verbose = True
+    qa.return_source_documents = True
+    # TODO 根据配置选择是否需要修改 prompt 模板
+    qa.combine_documents_chain.llm_chain.prompt = prompt
+
+    return qa
+
+
+def final_chain(qa: Runnable = None):
+    # branches = RunnableBranch(
+    #     (lambda p: p['topic'] == "qa", qa),
+    #     (lambda p: p['topic'] == "shopping",
+    #      RunnablePassthrough.assign(result=lambda x: '购物车功能还在抓紧开发中，敬请期待！')),
+    #     lambda x: x
+    # )
+    branches = RunnableBranch(
+        (lambda p: p['topic'] == "qa", qa),
+        (lambda p: p['topic'] == "shopping",
+         RunnablePassthrough.assign(request=itemgetter('query')) | RunnableLambda(finish_request)),
+        lambda x: x
+    )
+
+    chain = (
+            RunnablePassthrough.assign(topic=itemgetter('query') | RunnableLambda(determine_topic))
+            | branches
+    )
+    return chain.with_fallbacks(
+        fallbacks=[RunnableLambda(default_function)])
+    # return chain
+
+
+def default_function(x):
+    logger.error(f"正常流程调用出错. {x}")
+    return {"result": "您的请求有点小小难度，我需要升级大脑后才能给你服务:-("}
 
 
 class BookConsultant:
@@ -22,65 +83,35 @@ class BookConsultant:
         2. Help customer to shopping.eg, add book to shopping cart,
             delete book from shopping cart, view inventory, checkout
     """
-    llm: BaseLanguageModel
+    history_size: int
 
-    def __init__(self, llm: BaseLanguageModel = None, embedding: Embeddings = None) -> None:
-        qa = self._qa_chain(llm, embedding=embedding)
-        # self.shoppingcart_chain = self._shoppingcart_chain()
-        self.full_chain = self.build_fullchain(qa)
-
-    def build_fullchain(self, qa: Runnable):
-        classifier_function = convert_pydantic_to_openai_function(TopicClassifier)
-        llm = ChatOpenAI(temperature=0).bind(functions=[classifier_function], function_call={"name": "TopicClassifier"})
-        parser = PydanticAttrOutputFunctionsParser(pydantic_schema=TopicClassifier, attr_name="topic")
-        classifier_chain = llm | parser
-
-        chain_branch = RunnableBranch(
-            (lambda p: p['topic'] == "qa", qa),
-            (lambda p: p['topic'] == "shopping",
-             RunnablePassthrough.assign(result=lambda x: '购物车功能还在抓紧开发中，敬请期待！')),
-            lambda x: x
-        )
-
-        full_chain = (
-                RunnablePassthrough.assign(topic=itemgetter('query') | classifier_chain)
-                | chain_branch
-        )
-        return full_chain
-
-    def _qa_chain(self, llm: BaseLanguageModel = None, embedding: Embeddings = None) -> Runnable:
-        _embedding = embedding if embedding is not None else OpenAIEmbeddings()
-
-        db = FAISS.load_local('../jupyter/books', _embedding)
-        retriever = db.as_retriever(
-            search_type="similarity_score_threshold",
-            search_kwargs={"score_threshold": 0.7, "k": 3})
-        llm = llm if llm is not None else ChatOpenAI(temperature=0)
-        qa = RetrievalQA.from_chain_type(llm=llm, retriever=retriever, verbose=True)
-        qa.combine_documents_chain.verbose = True
-        qa.return_source_documents = True
-        # TODO 根据配置选择是否需要修改 prompt 模板
-        qa.combine_documents_chain.llm_chain.prompt = prompt
-
-        return qa
-
-    def _shoppingcart_chain(self) -> Runnable:
-        # return RunnablePassthrough.assign(result=lambda x: '购物车功能还在抓紧开发中，敬请期待！')
-        # TODO 购物车功能还在抓紧开发中，敬请期待！
-        pass
+    def __init__(self, history_size: int = 10) -> None:
+        self.history_size = history_size
+        self.qa = question_answer()
+        self.consultant = final_chain(self.qa)
+        # 历史对话记录
+        self.history = []
 
     def ask(self, question: str):
         logger.info(f"question: {question}")
 
-        reply = self.full_chain.invoke({"query": question})
+        reply = self.consultant.invoke({"query": question,
+                                        "history": ChatPromptTemplate.from_messages(self.history)})
         logger.info(f"reply: {reply}")
-        return reply["result"]
+        reply_content = reply["result"]
+        # 添加历史记录
+        self.history.append(HumanMessage(content=question))
+        self.history.append(AIMessage(content=reply_content))
+        # 保留最近的历史记录
+        self.history = self.history[-self.history_size:]
+        return reply_content
 
 
 class TopicClassifier(BaseModel):
     """Classify the topic of the user question
     qa: Reply to user inquiries for books and provide some purchasing suggestions
-    shopping: Assist customers in managing their shopping cart, including services such as adding products, deleting products, viewing inventory, and settling accounts
+    shopping: Assist customers in managing their shopping cart, including services such as adding products,
+        deleting products, viewing inventory, and settling accounts
     """
 
     topic: Literal["qa", "shopping"]
@@ -91,5 +122,8 @@ CONSULTANT = BookConsultant()
 
 if __name__ == '__main__':
     consultant = BookConsultant()
-    reply_result = consultant.ask("你这里有 Python 的书吗？")
+    # reply_result = consultant.ask("你这里有 Python 的书吗？")
+    # logger.info(f"reply result: {reply_result}")
+
+    reply_result = consultant.ask("帮我加入购物车")
     logger.info(f"reply result: {reply_result}")
--- booksales-consultant/booksales_consultant/gradio_ui.py
+++ booksales-consultant/booksales_consultant/gradio_ui.py
@@ -18,13 +18,15 @@ def launch():
         "我想成为计算机专家，有这方面的书吗",
         "我想了解一些东方哲学，有什么书可以推荐吗",
         "我想了解一些西方哲学，有什么书可以推荐吗",
+        "帮我将Python编程入门加入购物车",
+        "帮我结账"
     ]
     demo = gr.ChatInterface(fn=ask,
                             textbox=gr.Textbox(placeholder="请输入任何关于书的问题"),
                             examples=examples,
                             title="Book Consultant",
                             retry_btn=None, clear_btn=None, undo_btn=None,
-                            description="尊敬的客户，欢迎来到我们的书店！如果您有任何问题，欢迎随时向我们提问")
+                            description="尊敬的客户，欢迎来到我们的书店！如果您有任何问题，欢迎随时向我们提问。当有想买的书，可以让我帮你添加到购物车")
     logger.info("已启动 Book Consultant 服务")
     demo.launch(share=True, server_name="0.0.0.0")
 
